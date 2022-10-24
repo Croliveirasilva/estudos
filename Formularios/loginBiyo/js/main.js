ff2ab0128e13fdf65e3483224383e7924bb4043b
Carlos <dev47.osasbh@outlook.com>
2022-10-24 00:05:31 -0300

    Initial commit

---
+++ Formularios/loginBiyo/js/main.js
@@ -0,0 +1,19 @@
+const inputs= document.querySelectorAll("input");
+
+function addClass(){
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+
+}
+function removeClass(){
+  let parent= this.parentNode.parentNode;
+  if(this.value=="")
+  parent.classList.remove("focus");
+
+}
+
+inputs.forEach(input=>{
+  debugger
+input.addEventListener("focus",addClass);
+input.addEventListener("blur",removeClass);
+});
